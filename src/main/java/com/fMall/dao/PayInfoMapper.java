037ef8e81dc9c6dcec20fa5fe597bcfab114beef
gaocong0511 <qq370010866gc@gmail.com>
2018-04-25 21:07:04 +0800

    提交配置完成之后的项目

---
+++ src/main/java/com/fMall/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.fMall.dao;
+
+import com.fMall.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
---
+++ src/main/java/com/fMall/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.fMall.dao;
+
+import com.fMall.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}

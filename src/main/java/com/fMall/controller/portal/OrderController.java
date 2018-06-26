15145dea389ecd195971ec1848996caa5e0966da
gaocong0511 <qq370010866gc@gmail.com>
2018-06-26 23:02:35 +0800

    编写支付部分的代码

--- src/main/java/com/fMall/controller/portal/OrderController.java
+++ src/main/java/com/fMall/controller/portal/OrderController.java
@@ -1,14 +1,25 @@
 package com.fMall.controller.portal;
 
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.internal.util.AlipaySignature;
+import com.alipay.demo.trade.config.Configs;
 import com.fMall.common.Const;
 import com.fMall.common.ResponseCode;
 import com.fMall.common.ServerResponse;
 import com.fMall.pojo.User;
+import com.fMall.service.IOrderService;
+import com.google.common.collect.Maps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created by 高琮 on 2018/6/11.
@@ -17,18 +28,61 @@ import javax.servlet.http.HttpSession;
 @RequestMapping("/order/")
 public class OrderController {
 
+    private final IOrderService iOrderService;
+    private static final Logger logger=LoggerFactory.getLogger(OrderController.class);
+
+    @Autowired
+    public OrderController(IOrderService iOrderService) {
+        this.iOrderService = iOrderService;
+    }
+
     /**
      * 支付入口
-     * @param session HTTPSession对象
+     *
+     * @param session     HTTPSession对象
      * @param orderNumber 订单号
      * @return 统一返回对象
      */
-    public ServerResponse pay(HttpSession session,Long orderNumber,HttpServletRequest request){
-        User user=(User)session.getAttribute(Const.CURRENT_USER);
-        if(user==null){
-            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+    @RequestMapping("pay.do")
+    @ResponseBody
+    public ServerResponse pay(HttpSession session, Long orderNumber, HttpServletRequest request) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
         }
-        String path=request.getSession().getServletContext().getRealPath("upload");
-return null;
+        String path = request.getSession().getServletContext().getRealPath("upload");
+        return iOrderService.pay(orderNumber, user.getId(), path);
+    }
+
+
+    @RequestMapping("ali_pay_callback.do")
+    @ResponseBody
+    public Object alipayCallback(HttpServletRequest request) {
+        Map requestParams = request.getParameterMap();
+        Map<String,String> params=Maps.newHashMap();
+        for (Iterator iterator = requestParams.keySet().iterator(); iterator.hasNext(); ) {
+            String name = (String) iterator.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0, length = values.length; i < length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i] : valueStr + values[i] + ",";
+            }
+            params.put(name,valueStr);
+        }
+        logger.info("成功获取到支付宝回调信息中的参数sign:{},trade_status,参数:{},",params.get("sign"),
+                params.get("trade_status"),params.toString());
+        //验证回调的正确性  1.来自支付宝 2.并不是重复的通知
+        params.remove("sign_type");
+        try {
+            boolean alipayRSACheckV2=AlipaySignature.rsaCheckV2(params,Configs.getAlipayPublicKey(),"utf-8",Configs.getSignType());
+            if(alipayRSACheckV2){
+                //todo 处理正常的业务逻辑
+            }else {
+                //忽略这一回调信息
+            }
+        } catch (AlipayApiException e) {
+            logger.error("支付宝回调验证异常",e);
+        }
+        return null;
     }
 }
